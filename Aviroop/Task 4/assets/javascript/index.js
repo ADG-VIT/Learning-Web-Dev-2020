6050d6a88a8dd9feab6d2f9d895b8a44da3b4633
Aviroop Nandy <60533497+AviroopNandy@users.noreply.github.com>
2020-07-12 12:29:13 +0530

    Update index.js

--- Aviroop/Task 4/assets/javascript/index.js
+++ Aviroop/Task 4/assets/javascript/index.js
@@ -3,13 +3,18 @@ let flag = 0;
 function putData() {
     let movieName = document.getElementById('movieName').value;
     let output = '';
+    document.getElementById('output').innerHTML = output;
+    document.getElementById('movieImage').style.boxSizing = '';
+    document.getElementById('movieImage').style.border = '';
+    document.getElementById('movieImage').src = '';
     fetch('https://task4-api.herokuapp.com/')
         .then((res) => res.json())
         .then((data) => {
             const jsonData = data;
             console.log(jsonData.data.tv_shows[0].name);
             for(let i = 0; i < 100; i++) {
-                if(!(movieName.localeCompare(jsonData.data.tv_shows[i].name))) {
+                flag = 0;
+                if(!(movieName.localeCompare(jsonData.data.tv_shows[i].name, 'en', {sensitivity: 'base'}))) {
                     output += `
                         <ul class="list-group">
                             <li class="list-group-item" id="bg">Name: ${jsonData.data.tv_shows[i].name}</li>
@@ -30,7 +35,8 @@ function putData() {
             }
             if(flag == 1)
                 document.getElementById('output').innerHTML = output;
-            else
+            else {
                 alert('The show that you searched for is not available. Press OK to continue.');
+            }
         })
 }
