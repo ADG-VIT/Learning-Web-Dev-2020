07dbe96f7aa15f94b2ae07e265d18f75080d5d11
Ishan Bhardwaj <59660334+IB-14@users.noreply.github.com>
2020-09-05 17:36:33 +0530

    updated task4
    made changes in task4 js file

--- Ishan/Task 4/assets/index.js
+++ Ishan/Task 4/assets/index.js
@@ -1,23 +1,22 @@
 document.getElementById("SearchBox").addEventListener('click', getSeries);
 document.getElementById('in').addEventListener('keyup',function(event){
     if (event.keyCode === 13) {
-   event.preventDefault();
    document.getElementById("SearchBox").click();
   }
   });
-
+var output;
+var unfound= 'yes';
 function getSeries() {
-    prreventDefault();
-    console.log(9);
+    event.preventDefault();
     fetch('https://task4-api.herokuapp.com/')
     .then((res)=> res.json())
     .then((data)=> {
-        console.log('inside 2nd fetch')
-        var search = document.getElementById('SearchBox').nodeValue.toLowerCase;
         Series= data.data.tv_shows
+        var Search = document.getElementById('Search').value.toLowerCase().split(" ").join("");
         Series.forEach(element => {
-            if(search.localeCompare(element.name.toLowerCase())) {
-                output+= `
+            var s2=element.name.toLowerCase().split(" ").join("");;
+            if(Search.localeCompare(s2)===0) {
+             output= `
 
                 <div class='Poster'>
                     <img src= ${element.image_thumbnail_path}">
@@ -25,21 +24,33 @@ function getSeries() {
 
                 <div class='info'>
                     <ul>
-                        <li>Name: ${element.name}</li>  
-                        <li>Date : ${p.start_date} - ${p.end_date}</li>
-                        <li>Status: ${p.status}</li>
-                        <li>Country: ${p.country}</li>
-                        <li>Network: ${p.network}</li>
+                        <li><span style="font-size:larger;">Name:</span><span style="font-size:large; color: rgb(152, 203, 253)"> ${element.name}</span></li>  
+                        <li><span style="font-size:larger;">Date:</span><span style="font-size:large; color: rgb(152, 203, 253);"> ${element.start_date} - ${element.end_date}</span></li>
+                        <li><span style="font-size:larger;">Status:</span><span style="font-size:large; color: rgb(152, 203, 253);"> ${element.status}</span></li>
+                        <li><span style="font-size:larger;">Country:</span><span style="font-size:large; color: rgb(152, 203, 253);"> ${element.country}</span></li>
+                        <li><span style="font-size:larger;">Network:</span><span style="font-size:large; color: rgb(152, 203, 253);"> ${element.network}</span></li>
                         </ul>
                 </div>
     
-                `
+                `;
+
+                unfound= 'no';
+                document.getElementById('SearchResult').innerHTML=output;
+
             } else {
-                output= `<h3 style="text-align= center;">Sorry, we don't have ${document.getElementById('SearchBox').value} in our database :(</h3>`
+                output= `<h3 style="margin-top: -10% !important;">Sorry, we don't have <span style="color: dodgerblue;">"${document.getElementById('Search').value}"</span> in our database <span style="color: dodgerblue;">:(</span></h3>`
+                if(unfound==='no') {
+                    document.getElementById('Unfound').innerHTML="";
+                }
+                if(unfound==='yes') {
+                    document.getElementById('SearchResult').innerHTML="";
+                    document.getElementById('Unfound').innerHTML=output;
+                }
+                
             }
         })
 
-        document.getElementById('SearchResult').innerHTML=output;
+        
 
         });
         
